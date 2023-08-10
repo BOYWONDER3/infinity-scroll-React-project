dc0b0c3a32deb4292aea31f8103d83b8da5bf6d7
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-08-10 23:43:24 +0100

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,31 +2,42 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import "./styles.css";
 import { parseLinkHeader } from "./parseLinkHeader";
 
+const LIMIT = 50;
+
 export default function App() {
   const [photos, setPhotos] = useState([]);
-  const nextPhotoUrlRef = useRef()
+  const [isLoading, setIsLoading] = useState(false);
+  const nextPhotoUrlRef = useRef();
 
-  async function fetchPhotos(url, {overwrite = false } = {}) {
-    const res = await fetch(url)
-    nextPhotoUrlRef.current = parseLinkHeader(res.headers.get('Link')).next
-    const photos = await res.json()
-    if (overwrite) {
-      setPhotos(photos)
-    } else {
-      setPhotos(prevPhotos => {
-        return [...prevPhotos, ...photos]
-      })
+  async function fetchPhotos(url, { overwrite = false } = {}) {
+    setIsLoading(true);
+    try {
+      await new Promise(res => setTimeout(res, 2000))
+      const res = await fetch(url);
+      nextPhotoUrlRef.current = parseLinkHeader(res.headers.get("Link")).next;
+      const photos = await res.json();
+      if (overwrite) {
+        setPhotos(photos);
+      } else {
+        setPhotos((prevPhotos) => {
+          return [...prevPhotos, ...photos];
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
-  const imageRef = useCallback(image => {
-    if (image == null) return;
+  const imageRef = useCallback((image) => {
+    if (image == null || nextPhotoUrlRef.current == null ) return;
 
-    const observer = new IntersectionObserver(entries => {
+    const observer = new IntersectionObserver((entries) => {
       if (entries[0].isIntersectinging) {
-        fetchPhotos(nextPhotoUrlRef.current)
+        fetchPhotos(nextPhotoUrlRef.current);
         // TODO: Load next elements
-        observer.unobserve(image)
+        observer.unobserve(image);
       }
     });
 
@@ -34,7 +45,9 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    fetchPhotos("http://localhost:3000/photos?_page-1&_limit=10", { overwrite: true, })
+    fetchPhotos(`http://localhost:3000/photos-short-list?_page=1&_limit=${LIMIT}`, {
+      overwrite: true,
+    });
   }, []);
 
   return (
@@ -46,6 +59,14 @@ export default function App() {
           ref={index === photos.length - 1 ? imageRef : undefined}
         />
       ))}
+      {isLoading &&
+        Array.from({ length: LIMIT }, (_, index) => index).map((n) => {
+          return (
+          <div key={n} className="skeleton">
+            Loading...
+          </div>
+          )
+        })}
     </div>
   );
 }
